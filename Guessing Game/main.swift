64af3e99975fb8a015c15ece66d7c40496f55b26
Noah Smith <noahsmith@Interapts-MacBook-Pro-34.local>
2018-01-25 13:24:34 -0500

    Added user input logic

--- Guessing Game/main.swift
+++ Guessing Game/main.swift
@@ -6,7 +6,30 @@
 //  Copyright © 2018 Noah Smith. All rights reserved.
 //
 
-import Foundation
+import Cocoa
+
+//Generate a random number
+var randomNumber = Int(arc4random_uniform(100))
+var guess: Int?
+
+//Ask for user input
+print("Please guess a number.")
+
+//Take user input
+guess = Int(readLine()!)!
+
+if randomNumber == guess {
+    print("YOU WON!!!!!!!!! 🎉")
+} else if randomNumber > guess! {
+    print("The number is higher! 🤦‍♂️")
+} else if randomNumber < guess! {
+    print("The number is lower! 🤦‍♂️")
+}
+//Determine if it's wrong or right (Loop until correct)
 
 
+//If correct, ask if they want to play again
+
+
+//If incorrect, tell them the correct answer and ask them to play again
 
